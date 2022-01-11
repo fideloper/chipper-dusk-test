69e5398f81399828a69c825c20a03533d5f7cb32
Chris Fidao <fideloper@gmail.com>
2022-01-10 18:53:46 -0600

    dusk working with auth too

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dusk Testing
+
+
---
+++ tests/Browser/ExampleTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class ExampleTest extends DuskTestCase
+{
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->assertSee('Laravel');
+        });
+    }
+
+    public function testDashboard()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(1)
+                ->visit('/dashboard')
+                ->assertSee("You're logged in");
+        });
+    }
+}
